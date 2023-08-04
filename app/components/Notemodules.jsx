2cadc38ba9173d84254f704748b21d17b37f5e2b
abirtasrif <abirtasrif@gmail.com>
2023-08-04 23:17:59 +0600

    restructured data

---
+++ app/components/Notemodules.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import moment from "moment";
+
+const Notemodules = ({ note }) => {
+  const handleDelete = async () => {
+    const res = await fetch(`http://localhost:5000/api/notes/${note._id}`, {
+      method: "DELETE",
+    });
+    const json = await res.json();
+  };
+
+  return (
+    //statrting note card
+    <div key={note._id} id="note-card">
+      <div className="note-card-title">
+        <span>ID: {note._id}</span>
+        <h3>{note.title}</h3>
+        <p>
+          Last updated:
+          {moment(note.updatedAt).format("ddd DD-MMM-YY HH:mm A")}
+        </p>
+      </div>
+      <div className="note-card-body">
+        <p>{note.content}</p>
+      </div>
+      <div className="note-card-buttons">
+        <button>Update</button>
+        <button onClick={handleDelete}>Delete</button>
+        <button>Mark Done</button>
+      </div>
+    </div>
+  );
+};
+
+export default Notemodules;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
-import moment from "moment";
+
 import NoteEditor from "./components/NoteEditor";
 import { useProjectContext } from "./hooks/useProjectContext";
 import { BsPencilSquare } from "react-icons/bs";
+import Notemodules from "./components/Notemodules";
 
 const HomePage = () => {
   const { notes, dispatch } = useProjectContext();
@@ -29,30 +30,10 @@ const HomePage = () => {
         {/* new note card */}
         <div id="note-card" className="new-note">
           <BsPencilSquare />
-          <p className="para">Add a New Note</p>
+          <p>Add a New Note</p>
         </div>
         {notes &&
-          notes.map((note) => (
-            //statrting note card
-            <div key={note._id} id="note-card">
-              <div className="note-card-title">
-                <span>ID: {note._id}</span>
-                <h3>{note.title}</h3>
-                <p>
-                  Last updated:
-                  {moment(note.updatedAt).format("ddd DD-MMM-YY HH:mm A")}
-                </p>
-              </div>
-              <div className="note-card-body">
-                <p>{note.content}</p>
-              </div>
-              <div className="note-card-buttons">
-                <button>Update</button>
-                <button>Delete</button>
-                <button>Mark Done</button>
-              </div>
-            </div>
-          ))}
+          notes.map((note) => <Notemodules key={note._id} note={note} />)}
       </div>
 
       <NoteEditor />
